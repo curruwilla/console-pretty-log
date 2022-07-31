677bb641d55ea03039010029b48bd51274064780
William Alvares <curruwilla@gmail.com>
2022-07-31 01:41:40 -0300

    First commit - console-pretty-log

---
+++ src/Background.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ConsolePrettyLog;
+
+/**
+ * Class Color
+ * @package ConsolePrettyLog
+ * @author William Alvares <30/07/2022 22:40>
+ */
+abstract class Background
+{
+    /**
+     *
+     */
+    public const RED = 'redbg';
+    /**
+     *
+     */
+    public const GREEN = 'greenbg';
+    /**
+     *
+     */
+    public const YELLOW = 'yellowbg';
+    /**
+     *
+     */
+    public const BLUE = 'bluebg';
+    /**
+     *
+     */
+    public const MAGENTA = 'magentabg';
+    /**
+     *
+     */
+    public const CYAN = 'cyanbg';
+    /**
+     *
+     */
+    public const LIGHTGREY = 'lightgreybg';
+}
---
+++ src/Color.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ConsolePrettyLog;
+
+/**
+ * Class Color
+ * @package ConsolePrettyLog
+ * @author William Alvares <30/07/2022 22:40>
+ */
+abstract class Color
+{
+    /**
+     *
+     */
+    public const RED = 'red';
+    /**
+     *
+     */
+    public const GREEN = 'green';
+    /**
+     *
+     */
+    public const YELLOW = 'yellow';
+    /**
+     *
+     */
+    public const BLUE = 'blue';
+    /**
+     *
+     */
+    public const MAGENTA = 'magenta';
+    /**
+     *
+     */
+    public const CYAN = 'cyan';
+    /**
+     *
+     */
+    public const WHITE = 'white';
+}
